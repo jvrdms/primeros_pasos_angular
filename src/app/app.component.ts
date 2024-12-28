045519ac017a847ac988b8b7326158e8f741699b
jvrdms <jvrdms@gmail.com>
2024-12-28 13:25:40 +0100

    angular-cli-ghpages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeros pasos en angular';
+  title = 'primeros pasos en Angular';
 }
